2002b7af9bf0dc7aaac82a62e80b973aea5371c9
Anirup Patnaik <anirup049@gmail.com>
2021-06-06 11:51:17 +0530

    Pattern6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Pattern1 from './components/Pattern1';
 import Pattern2 from './components/Pattern2';
 import Pattern3 from './components/Pattern3';
 import Pattern4 from './components/Pattern4';
+import Pattern5 from './components/Pattern5';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Pattern2 />
       <Pattern3 />
       <Pattern4 />
+      <Pattern5 />
     </div>
   );
 }
